6dd32c3eef998c65848e585281ae66b84d1b5f00
Mahmoud <mahmoudkota97@outlook.com>
2023-06-23 14:27:41 +0300

    finish web view perfectly

--- lib/view/web_view_screen.dart
+++ lib/view/web_view_screen.dart
@@ -1,5 +1,10 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:news_app_store/widgets/default_custom_text.dart';
+import 'package:share_plus/share_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import '../widgets/list_tile_widget.dart';
 
 class WebViewScreen extends StatefulWidget {
   const WebViewScreen({Key? key, required this.url}) : super(key: key);
@@ -11,6 +16,8 @@ class WebViewScreen extends StatefulWidget {
 
 class _WebViewScreenState extends State<WebViewScreen> {
   late final WebViewController controller;
+  var scaffoldKey = GlobalKey<ScaffoldState>();
+  var loadingPercentage = 0;
 
   @override
   void initState() {
@@ -29,19 +36,142 @@ class _WebViewScreenState extends State<WebViewScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.url),
-          // actions: [
-          //   PopupMenuButton(itemBuilder: (context){
-          //  /// to make share and show in browser
-          //   })
-          // ],
-        ),
-        body: WebViewWidget(
-          controller: controller,
-        ));
+    return WillPopScope(
+      onWillPop: () async {
+        if (await controller.canGoBack()) {
+          controller.goBack();
+          return false;
+        }
+        else{
+          return true;
+        }
+      },
+      child: Scaffold(
+          key: scaffoldKey,
+          appBar: AppBar(
+            title: Text(widget.url),
+            actions: [
+              GestureDetector(
+                onTap: () {
+                  showOptions();
+                },
+                child: const Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Icon(Icons.more_vert),
+                ),
+              )
+            ],
+          ),
+          body: Column(
+            children: [
+              if (loadingPercentage < 100)
+                LinearProgressIndicator(
+                  color: Colors.blue,
+                  value: loadingPercentage / 100.0,
+                ),
+              Expanded(
+                child: WebViewWidget(
+                  controller: controller
+                    ..setNavigationDelegate(NavigationDelegate(
+                      onPageStarted: (url) {
+                        setState(() {
+                          loadingPercentage = 0;
+                        });
+                      },
+                      onProgress: (progress) {
+                        setState(() {
+                          loadingPercentage = progress;
+                        });
+                      },
+                      onPageFinished: (url) {
+                        setState(() {
+                          loadingPercentage = 100;
+                        });
+                      },
+                      onNavigationRequest: (NavigationRequest request) {
+                        return NavigationDecision.navigate;
+                      },
+                    )),
+                ),
+              ),
+            ],
+          )),
+    );
   }
-  void showSheet(){}
 
+  void showOptions() {
+    double size = MediaQuery.of(context).size.height;
+    scaffoldKey.currentState?.showBottomSheet(
+      (context) => Container(
+        decoration: BoxDecoration(
+            color: Theme.of(context).cardColor,
+            borderRadius: const BorderRadius.only(
+              topRight: Radius.circular(25),
+              topLeft: Radius.circular(25),
+            )),
+        padding: const EdgeInsets.all(10),
+        height: size * 0.32,
+        width: double.infinity,
+        child: Column(
+          children: [
+            Row(
+              children: [
+                const Spacer(),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: DefaultCustomText(
+                    text: 'More Options',
+                  ),
+                ),
+                const Spacer(),
+                Align(
+                  alignment: AlignmentDirectional.centerEnd,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Icon(Icons.close),
+                  ),
+                ),
+              ],
+            ),
+            const Divider(
+              thickness: 3,
+            ),
+            ListTileWidget(
+                text: 'Share',
+                function: () {
+                  try {
+                    Share.share(widget.url, subject: 'Look what I made!');
+                  } catch (e) {
+                    log(e.toString());
+                  }
+                },
+                icon: Icons.redo),
+            ListTileWidget(
+                text: 'Open in Browser',
+                function: () async {
+                  final Uri _url = Uri.parse(widget.url);
+
+                  if (!await launchUrl(_url)) {
+                    throw Exception('Could not launch $_url');
+                  }
+                },
+                icon: Icons.link),
+            ListTileWidget(
+                text: 'Refresh',
+                function: () {
+                  try {
+                    controller.reload();
+                    Navigator.pop(context);
+                  } catch (e) {
+                    log(e.toString());
+                  }
+                },
+                icon: Icons.refresh),
+          ],
+        ),
+      ),
+    );
+  }
 }
